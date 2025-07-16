f35fdc719ca717f3008c4c965a4f4acb7bd45378
Sora3352 <2401003@s.asojuku.ac.jp>
2025-07-16 10:51:23 +0900

    f

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
 //読み込み完了後に実行する
 $(function() {
-    //#btnがクリックされたとき
-    $('#btn').on('click', function(){
+    // #searchBtnがクリックされたとき
+    $('#searchBtn').on('click', function(){
         $.ajax({
-            //リクエスト先のURLを設定
-
-            //レスポンスのデータ形式を設定
-
+            // zipcloudのAPIエンドポイント
+            url: 'https://zipcloud.ibsnet.co.jp/api/search',
+            dataType: 'json',
+            data: {
+                zipcode: $('#zipcode').val()
+            }
         }).done(function(data){
-            //通信が成功したときの処理
+            console.log(data); // レスポンス内容を確認
             if(data.results){
-                //データが取得できたとき、setData関数を呼び出す
-
+                setData(data.results[0]); // zipcloudは配列で返す
             }else{
                 alert("該当するデータが見つかりませんでした");
             }
-        }).fail(function(data){
-            //通信が失敗したときの処理
+        }).fail(function(){
             alert("通信に失敗しました");
         });
     });
 });
 
 function setData(data){
-    //取得データを各HTML要素に代入
-
+    // zipcloudのレスポンス仕様に合わせて代入
+    $('#prefecture').val(data.address1);
+    $('#city').val(data.address2);
+    $('#address').val(data.address3);
 }
